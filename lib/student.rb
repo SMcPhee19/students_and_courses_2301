a48856bc50e8ca592b0d8cc8b14a45b695e44ca8
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-15 09:38:40 -0700

    Complete Iteration 1

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,20 @@
 class Student
-  def initialize(attributes)
-    
+  attr_reader :name,
+              :age,
+              :scores
+
+  def initialize(student_data)
+    @name = student_data[:name]
+    @age = student_data[:age]
+    @scores = []
   end
+
+  def log_score(assignment_score)
+    @scores << assignment_score
+  end
+  
+  def grade
+    @scores.sum.to_f / @scores.size
+  end
+  
 end
