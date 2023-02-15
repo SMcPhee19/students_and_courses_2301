14d7771d0f639d70b4de90482b4fe065bf4f3bad
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-15 09:56:31 -0700

    Add methods for iteration 2

--- lib/course.rb
+++ lib/course.rb
@@ -1,1 +1,25 @@
+class Course
+  attr_reader :name,
+              :capacity,
+              :students
 
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+  end
+
+  def full?
+    if @students.count < 2
+      false
+    else
+      true  
+    end
+  end
+
+  def enroll
+    
+  end
+
+
+end
